31a29831cca65bba445053efc13f14e0a107b76f
artdgn <arthurdgn@gmail.com>
2020-12-26 00:52:48 +1100

    add world progress world-SIR summary stats line, fix zoom issue

--- _notebooks/2020-04-08-covid19-world-progress.py
+++ _notebooks/2020-04-08-covid19-world-progress.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.2
+#       jupytext_version: 1.6.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -16,7 +16,7 @@
 
 # + [markdown] papermill={"duration": 0.013695, "end_time": "2020-03-27T06:31:15.895652", "exception": false, "start_time": "2020-03-27T06:31:15.881957", "status": "completed"} tags=[]
 # # World progress projections
-# > Visualising stacked projections for all countries.
+# > Visualising stacked progress & projections for all countries.
 #
 # - permalink: /covid-world-progress/
 # - image: images/world-infected.png
@@ -38,15 +38,7 @@ df_all = covid_data.table_with_projections()
 
 #hide_input
 from IPython.display import Markdown
-Markdown(f"***Based on data up to: {covid_data.cur_date}***")
-
-# ## For details per country see [main notebook](/pages/covid-progress-projections/)
-
-# ## World projections from country models (all countries stacked)
-# The outputs of the models for all countries in stacked plots.
-# > Tip: Hover the mouse of the area to see which country is which and the countries S/I/R ratios at that point. 
-#
-# > Tip: The plots are zoomable and draggable.
+Markdown(f"*Based on data up to:* ***{covid_data.cur_date}***")
 
 # +
 #hide
@@ -64,10 +56,28 @@ for c in df_tot.columns[df_alt.dtypes == float]:
 df_tot = df_tot.reset_index()
 df_tot.columns = [c.replace('.', '-') for c in df_tot.columns]
 
+#hide_input
+df_now = df_tot[df_tot['day'] == 0]
+pop = df_now['population'].sum()
+s_now = df_now['Susceptible-total'].sum() / pop
+i_now = df_now['Infected-total'].sum() / pop
+r_now = df_now['Removed-total'].sum() / pop
+Markdown("### World totals:\n"
+         f"Infected &#128567;: **{i_now:.1%}**, "
+         f"Removed &#128532;: **{r_now:.1%}**, "
+         f"Susceptible &#128543;: **{s_now:.1%}**")
+
+# ### Progress & projections from country models (all countries stacked)
+# The outputs of the models for all countries in stacked plots. For details per country see [main notebook](/pages/covid-progress-projections/)
+# > Tip: Hover the mouse of the area to see which country is which and the countries S/I/R ratios at that point. 
+#
+# > Tip: The plots are zoomable and draggable.
+
 # +
 #hide
 # filter by days
-df_tot = df_tot[df_tot['day'].between(-30, 30) | (df_tot['day'] % 10 == 0)]
+days = 30
+df_tot = df_tot[df_tot['day'].between(-days, days) | (df_tot['day'] % 10 == 0)]
 
 # filter out noisy countries for actively infected plot:
 df_filt = covid_data.filter_df(df_all)
@@ -84,11 +94,12 @@ today_line = (alt.Chart(pd.DataFrame({'x': [0]}))
                   .encode(x='x', size=alt.value(1)))
 
 # make plot
-max_y = df_tot_filt[df_tot_filt['day']==30]['Infected-total'].sum()
+max_y = (df_tot_filt[df_tot_filt['day'].between(-days, days)]
+         .groupby('day')['Infected-total'].sum().max())
 stacked_inf = alt.Chart(df_tot_filt).mark_area().encode(
     x=alt.X('day:Q',
             title=f'days relative to today ({covid_data.cur_date})',
-            scale=alt.Scale(domain=(-30, 30))),
+            scale=alt.Scale(domain=(-days, days))),
     y=alt.Y("Infected-total:Q", stack=True, title="Number of people",
            scale=alt.Scale(domain=(0, max_y))),
     color=alt.Color("Country/Region:N", legend=None),
@@ -101,11 +112,11 @@ stacked_inf = alt.Chart(df_tot_filt).mark_area().encode(
 
 # +
 #hide_input
-max_y = df_tot_filt[df_tot_filt['day']==30]['Removed-total'].sum()
+max_y = df_tot_filt[df_tot_filt['day']==days]['Removed-total'].sum()
 stacked_rem = alt.Chart(df_tot_filt).mark_area().encode(
     x=alt.X('day:Q',
             title=f'days relative to today ({covid_data.cur_date})',
-            scale=alt.Scale(domain=(-30, 30))),
+            scale=alt.Scale(domain=(-days, days))),
     y=alt.Y("Removed-total:Q", stack=True, title="Number of people",
            scale=alt.Scale(domain=(0, max_y))),
     color=alt.Color("Country/Region:N", legend=None),
@@ -119,11 +130,11 @@ stacked_rem = alt.Chart(df_tot_filt).mark_area().encode(
 
 # +
 #hide_input
-max_y = df_tot[df_tot['day']==1]['Susceptible-total'].sum()
+max_y = df_tot[df_tot['day']==-days]['Susceptible-total'].sum()
 stacked_sus = alt.Chart(df_tot).mark_area().encode(
     x=alt.X('day:Q',
             title=f'days relative to today ({covid_data.cur_date})',
-            scale=alt.Scale(domain=(-30, 30))),
+            scale=alt.Scale(domain=(-days, days))),
     y=alt.Y("Susceptible-max-total:Q", stack=True, 
             scale=alt.Scale(domain=(0, max_y)),
            title='Number of people'),
